23a606122cb867f1b594292afa76ea70b27f25ef
snailrose <38305297+snailrose@users.noreply.github.com>
2019-07-17 03:34:31 -0600

    Updated the configure files for Utils

--- CMake/ConfigureUtils.cmake
+++ CMake/ConfigureUtils.cmake
@@ -3,7 +3,6 @@ set(Utils_USE_SIGNED_SIZE_T     FALSE           CACHE BOOL      "Define SKsize t
 set(Utils_USE_STD_STRING_FUNCS  TRUE            CACHE BOOL      "Use system strlen, strcpy ...")
 set(Utils_USE_DEBUG_ASSERT      TRUE            CACHE BOOL      "Enable debug asserts")
 set(Utils_USE_ITERATOR_DEBUG    FALSE           CACHE BOOL      "Enable state checks for iterators")
-set(Utils_OP_CHECKS			    FALSE           CACHE BOOL      "Remove state check macros.")
 set(Utils_USE_COMPILER_CHECKS   FALSE           CACHE BOOL      "Enable compile time asserts.")
 set(Utils_NO_DEBUGGER           TRUE            CACHE BOOL      "Send calls to skPrintf to printf vs skDebugger::report")
 
