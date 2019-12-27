3fa7a1a98ab64d47de8fc9846edeb97f3b5aac13
CharlesCarley <snailrose@gmail.com>
2019-12-27 11:04:34 -0700

    Test Commit

--- skStringConverter.cpp
+++ skStringConverter.cpp
@@ -23,7 +23,6 @@
   3. This notice may not be removed or altered from any source distribution.
 -------------------------------------------------------------------------------
 */
-
 #include "skStringConverter.h"
 
 skString skStringConverter::BLANK = "";
